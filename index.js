ce76da6e230dacf7bcc2e3cfb83eb530d6df43fc
unknown <gabelwarega@gmail.com>
2025-04-04 12:09:33 +0300

    added the grid size dimensions

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ function gridFunction(row, col){
     // loop through the number of rows and columns
     for(let i =0; i < row * col; i++){
         // create a div element
-        const grid = document.createElement("div");
+        const gridItem = document.createElement("div");
         // add class to the div element
-        grid.classList.add("grid-item");
+        gridItem.classList.add("grid-item");
         // add text to the div element
-        grid.innerText = `${i + 1}`;
-        container.appendChild(grid); // append the div to the container
+        //grid.textContent = ' ';
+        container.appendChild(gridItem); // append the div to the container
     }
 }
-
 // call the function with the number of rows and columns
-gridFunction(4, 4);
+gridFunction(16, 16);
