e4f1999c8e93ac023e4a1c4e512f42b6b665e17a
unknown <gabelwarega@gmail.com>
2025-04-04 16:45:34 +0300

    added the functionality of the buttons

--- index.js
+++ index.js
@@ -7,26 +7,57 @@ function gridFunction(row, col){
         const gridItem = document.createElement("div");
         // add class to the div element
         gridItem.classList.add("grid-item");
-        // add text to the div element
-        //grid.textContent = ' ';
         container.appendChild(gridItem); // append the div to the container
     }
 }
 // call the function with the number of rows and columns
 gridFunction(16, 16);
 
-const gridHover = document.querySelectorAll(".grid-item");
+// DOM Manipulation
+const titles = document.querySelector(".title");
+titles.textContent = "Etch - a - Sketch";
+titles.style.fontSize = "50px";
+titles.style.color = "black"
+titles.fontFamilyt ="auto"
+
+// set the color change on hover for the grids function
+function gridColorChange(){
+const gridHover = document.querySelectorAll(".grid-item"); // all the grids
+
 // loop through the grid items
 gridHover.forEach(function(gridHover){
     // add event listener to the grid items
     gridHover.addEventListener("mouseover", function(){
         const colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink","brown", "gray", "black","maroon", "navy", "teal", "lime", "olive", "coral","ginger", "salmon", "khaki", "plum", "turquoise"];
         // get a random color from the array
-        const randomColor = Math.floor(Math.random() * colors.length);        
+        const randomColor = Math.floor(Math.random() * colors.length);   
         // change the background color to black on hover
         gridHover.style.backgroundColor = colors[randomColor];
         gridHover.style.opacity = 1;
-        gridHover.style.transition = "background-color 0.5s ease-in-out";
+        gridHover.style.transition = "background-color 0.3s ease-in-out";
         gridHover.style.opacity = 0.5;
     });
-});
+})}
+gridColorChange()
+
+
+// DOM for the buttons
+const gridResize = document.querySelector('.resize');
+gridResize.addEventListener('click', function(){
+    let userInput = prompt('Enter number of squares per side (max 100)');
+    // set conditions
+    if (userInput > 0 && userInput <= 100) {
+        gridFunction(userInput, userInput); // Call gridFunction with valid input
+        let result = gridColorChange()
+        return result
+
+    } else {
+        alert('Please try a value between 1 and 100');
+    }
+})
+
+// grid reset functionality
+const gridReset = document.querySelector(".reset");
+gridReset.addEventListener('click', function(){
+    gridFunction(16,16)
+})
