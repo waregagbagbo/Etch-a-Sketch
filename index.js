c7beaa505599469f419d06a813375263d22b4289
unknown <gabelwarega@gmail.com>
2025-04-05 16:23:35 +0300

    fixed the dynamic size manipulations

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 function gridFunction(row,col){
     const container = document.querySelector(".container");
-    container.innerHTML = "" // clear existing grid
+    container.innerHTML = '' // clear existing grid
     // loop through the number of rows and columns
     for(let i=0; i < row * col; i++){
         // create a div element
         const gridItem = document.createElement("div");
+
         // add class to the div element
         gridItem.classList.add("grid-item");
+
+         // Dynamically set width and height
+         gridItem.style.width = `calc(100% / ${col})`;
+         gridItem.style.height = `calc(100% / ${row})`;       
+         
         container.appendChild(gridItem); // append the div to the container
     }
 }
@@ -15,16 +21,16 @@ gridFunction(16,16);
 
 // DOM Manipulation
 const titles = document.querySelector(".title");
-titles.textContent = "Etch - a - Sketch";
+titles.textContent = "Etch-a- Sketch";
 titles.style.fontSize = "50px";
-titles.style.color = "black"
-titles.fontFamilyt ="auto"
-titles.style.color ="white"
+titles.style.color = "black";
+titles.style.color ="white";
 
 // set the color change on hover for the grids function
-function gridColorChange(){
-const gridHover = document.querySelectorAll(".grid-item"); // all the grids
 
+function gridColorChange(){
+
+const gridHover = document.querySelectorAll(".grid-item"); // all the grids
 // loop through the grid items
 gridHover.forEach(function(gridHover){
     // add event listener to the grid items
@@ -45,19 +51,19 @@ gridColorChange()
 // DOM for the buttons
 const gridResize = document.querySelector('.resize');
 gridResize.addEventListener('click', function(){
-    let userInput = prompt('Enter number of squares per side (max 100)');
+    let userInput = prompt(parseInt('Enter number of squares per side (max 100)'));
     // set conditions
     if (userInput > 0 && userInput <= 100) {
-        const container = document.querySelector('.container')
-        container.innerHTML = "" // clear existing grid
-        gridFunction(userInput, userInput); // Call gridFunction with valid input
+
+        gridFunction(userInput,userInput); // Call gridFunction with valid input
     } else {
         alert('Please try a value between 1 and 100');
     }
 })
 
+
 // grid reset functionality
 const gridReset = document.querySelector(".reset");
 gridReset.addEventListener('click', function(){
-    gridFunction(16, 16); // Reset to default grid size
+    gridFunction(16,16); // Reset to default grid size
 })
